c6b648ead9e14e4b5cd02e2ac7712320ed59c0bb
Azamat Namiyaly <ANamiyaly@admins-MBP-3.bbrouter>
2023-12-08 03:50:17 +0600

    add test

--- core/db.js
+++ core/db.js
@@ -2,5 +2,6 @@ const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize('mysql://root:h5GGbac6dBh44cHFfEc-EbDCFeCagFf6@monorail.proxy.rlwy.net:23385/railway')
 
+//test
 
 module.exports.sequelize = sequelize
